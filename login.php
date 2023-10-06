60d3ef6eca1e43548caed508aa6f521f5655ad6e
Prajwalbh123 <98490630+Prajwalbh123@users.noreply.github.com>
2023-10-06 14:47:41 -0400

    Add files via upload

---
+++ login.php
@@ -0,0 +1,29 @@
+<?php
+// Connect to MySQL database
+$servername = "localhost";
+$username = "your_username";
+$password = "your_password";
+$dbname = "your_database_name";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+// Get form data
+$email = $_POST["email"];
+$password = $_POST["password"];
+
+// Insert form data into database
+$sql = "INSERT INTO users (email, password) VALUES ('$email', '$password')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
